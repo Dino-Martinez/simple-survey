818763a821f152d362ef9bfad685c785c1323f11
Dino-Martinez <Dino.Martinez.514@gmail.com>
2022-03-14 15:30:28 -0400

    [Update] app.js
    Added middleware for static files as well as setting our app view engine

--- src/app.js
+++ src/app.js
@@ -4,11 +4,22 @@ const helmet = require('helmet') // Security and HTTP header middleware
 const cors = require('cors') // Handling CORS for accessible APIs
 const morgan = require('morgan') // Request logging
 const compression = require('compression') // GZIP middleware for compressing responses
+const handlebars = require('express-handlebars')
+const path = require('path')
 
 // App
 const app = express()
 
+// View Engine
+app.engine('hbs', handlebars.engine({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
 // Middleware
+app.use(express.static(path.join(__dirname, 'public'))) // Require static assets from public folder
 app.use(morgan('common'))
 app.use(helmet())
 app.use(cors())
