63a1e0fbd767c3f212d4f9c4d2904fff813c791b
Dino-Martinez <Dino.Martinez.514@gmail.com>
2022-03-15 03:03:06 -0400

    [Update] routes/index
    Added post route to receive the durvey creation data

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 module.exports = (app, connectDb) => {
-
   connectDb()
   app.get('/', (req, res) => {
     res.render('home')
@@ -9,33 +8,38 @@ module.exports = (app, connectDb) => {
     res.render('create')
   })
 
-  app.get('/respond/:id', (req, res) => {
-    console.log("Dynamic Survey Route.")
+  app.post('/create', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
+  })
 
-    exampleSurvey = {
-      name: "Is this a simple survey?", 
+  app.get('/respond/:id', (req, res) => {
+    console.log('Dynamic Survey Route.')
+
+    const exampleSurvey = {
+      name: 'Is this a simple survey?',
       questions: [
         {
-          prompt: "On a scale from 1 to 10 how hard is this survey?",
-          type: "text",
+          prompt: 'On a scale from 1 to 10 how hard is this survey?',
+          type: 'text'
         },
         {
-          prompt: "What are good alternative names to simple survey?",
-          type: "radio",
-          options: ["simpl-serve", "simple surv(e with a tilde)", "nothing, stop doing the most."]
+          prompt: 'What are good alternative names to simple survey?',
+          type: 'radio',
+          options: ['simpl-serve', 'simple surv(e with a tilde)', 'nothing, stop doing the most.']
         },
         {
-          prompt: "How many hours will Dino spend on the front-end?",
-          type: "checkbox",
-          options: ["+5 min", "+30 min", "+1 hr", "+3 hr"]
+          prompt: 'How many hours will Dino spend on the front-end?',
+          type: 'checkbox',
+          options: ['+5 min', '+30 min', '+1 hr', '+3 hr']
         },
         {
-          prompt: "How many surveys could a woodchuck chuck, if a woodchuck could chuck wood?",
-          type: "number"
+          prompt: 'How many surveys could a woodchuck chuck, if a woodchuck could chuck wood?',
+          type: 'number'
         }
       ]
     }
 
-    res.render('survey', {survey: exampleSurvey})
+    res.render('survey', { survey: exampleSurvey })
   })
 }
