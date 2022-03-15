e29d225424855844da3b81d79ae70a29aebc0bd5
Dino-Martinez <Dino.Martinez.514@gmail.com>
2022-03-15 03:07:13 -0400

    [Update] public/script
    Added script for appending new prompts to the survey creation form

---
+++ src/public/script.js
@@ -0,0 +1,35 @@
+// function addPrompt () {
+//   const label = document.createElement('label')
+//   createForm.appendChild(label)
+// }
+
+// const createForm = document.querySelector('#create-form')
+// // const numInputs = document.querySelectorAll('input').length
+// addPromptButton.on('click', addPrompt)
+
+const promptGroup = document.querySelector('#prompt-group')
+const addPromptButton = document.querySelector('#add-prompt')
+let numPrompts = 0
+addPromptButton.addEventListener('click', (e) => {
+  const prompt = document.createElement('div')
+  prompt.classList.add('prompt')
+  prompt.innerHTML = `
+  <label for="prompt">Prompt</label>
+  <input type="text" name="prompts[${numPrompts}][prompt]" id="prompt-${numPrompts}">
+
+  <label for="type">Type</label>
+  <select name="prompts[${numPrompts}][type]" id="type-${numPrompts}">
+    <option value="text">Text</option>
+    <option value="number">Number</option>
+  </select>
+  `
+  numPrompts++
+  promptGroup.appendChild(prompt)
+})
+
+const type = document.querySelector('#type')
+if (type) {
+  type.addEventListener('change', (e) => {
+    console.log(e.target.value)
+  })
+}
