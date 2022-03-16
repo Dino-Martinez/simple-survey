1aa53e3f7f70904a4775f2c9f749097bb47dca37
tristann3 <tristan15thompson@gmail.com>
2022-03-16 11:34:14 -0400

    implemented survey model

---
+++ src/models/survey.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const SurveySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    questions: [
+      {
+        prompt: { type: String, required: true },
+        type: { type: String, required: true },
+        options: { type: Array, required: false }
+      }
+    ]
+  }
+)
+
+module.exports = mongoose.model('Survey', SurveySchema)
