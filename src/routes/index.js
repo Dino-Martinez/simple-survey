7660b3d9ef475303c50430df790304f8f4ca9a54
tristann3 <tristan15thompson@gmail.com>
2022-03-18 16:38:44 -0400

    users can take suveys and save them to the databse

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const Survey = require('../models/survey')
+const mongoose = require('mongoose')
 
 module.exports = (app, connectDb) => {
   // Establish connection to Database
   connectDb()
+
+  async function fetchSurvey (req) {
+    return await Survey.findById(req.params.id).lean()
+  }
 
   app.get('/', (req, res) => {
     res.render('home')
@@ -24,9 +29,6 @@ module.exports = (app, connectDb) => {
 
   app.get('/respond/:id', (req, res) => {
     console.log(req.params.id)
-    async function fetchSurvey (req) {
-      return await Survey.findById(req.params.id).lean()
-    }
 
     fetchSurvey(req)
       .then(function (survey) {
@@ -35,6 +37,35 @@ module.exports = (app, connectDb) => {
   })
 
   app.post('/respond/:id', (req, res) => {
-    res.send({ status: 200, message: 'Route Got hit, nothing saved.' })
+    fetchSurvey(req)
+      .then(function (survey) {
+        const questions = survey.questions
+        const id = mongoose.Types.ObjectId(req.params.id)
+        questions.forEach((question, x) => {
+          // case: radio, check
+          if (question.options) {
+            const arr = []
+            question.options.forEach((option, y) => {
+              if (req.body.questions[x].includes(option)) {
+                arr.push(true)
+              } else {
+                arr.push(false)
+              }
+            })
+            question.responses.push(arr)
+          } else {
+            // base case : text, number
+            question.responses.push(req.body.questions[x])
+          }
+        })
+
+        Survey.findByIdAndUpdate({ _id: id }, { questions: questions }, function (err, result) {
+          if (err) {
+            res.send(err)
+          } else {
+            res.send({ status: 200, message: 'Responses saved successfully!!' })
+          }
+        })
+      })
   })
 }
